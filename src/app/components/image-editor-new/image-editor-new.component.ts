118504909ede3dfd7526e456122e465978f94740
single-batman <batman.st2012@gmail.com>
2021-05-13 05:12:45 +0300

    Finish image-editor (TODO: integrated)

--- src/app/components/image-editor-new/image-editor-new.component.ts
+++ src/app/components/image-editor-new/image-editor-new.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { PixieImageEditorComponent } from '../pixie-image-editor/pixie-image-editor.component';
 
 @Component({
   selector: 'app-image-editor',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./image-editor-new.component.css']
 })
 export class ImageEditorNewComponent implements OnInit {
-
+  @ViewChild("imageEditor")
+  imageEditor: PixieImageEditorComponent;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  triggerSave(): void {
+    this.imageEditor.saveTrigger();
+  }
+
+  onSave(data): void {
+    console.log(data);
+  }
 }
