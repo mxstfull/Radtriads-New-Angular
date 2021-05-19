9a5cb2f62f49a0ef74a2d23ebe801df5945a9997
single-batman <batman.st2012@gmail.com>
2021-05-19 16:31:23 -0400

    update url

--- src/app/shared/appSettings.ts
+++ src/app/shared/appSettings.ts
@@ -1,4 +1,4 @@
 export class AppSettings {
-    public static backendURL='http://15.222.57.63/';
-    public static frontendURL='http://15.222.57.63:4200';
+    public static backendURL='http://15.222.57.63:8000/';
+    public static frontendURL='http://15.222.57.63';
  }
